b2234b76e90a8d73bd6d05a440952d8b61f4f5d5
Martin Lowe <martinlowe673@gmail.com>
2024-06-14 08:06:14 +0100

    user image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,6 +34,12 @@ export default function Home() {
                  </div>
 
             </ul>
+            <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
+              <div className="flex -space-x-4">
+                <img className="inline-bloxk h-10 w-10 rounded-full ring-2 ring-slate-100" src="/users/user-1.png" alt="user image"  />
+                <img className="inline-bloxk h-10 w-10 rounded-full ring-2 ring-slate-100" src="/users/user-1.png" alt="user image"  />
+              </div>
+            </div>
           </div>
         </div>
       </MaxWidthWrapper>
