4096fab237845851d45e45344819ec52941575e0
Martin Lowe <martinlowe673@gmail.com>
2024-06-23 16:01:34 +0100

    button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link"
 import MaxWidthWrapper from "./MaxWidthWrapper"
+import { buttonVariants } from "./ui/button"
 
 const Navbar = () =>{
+    const user = undefined
 return (
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
         <MaxWidthWrapper>
@@ -10,7 +12,12 @@ return (
                     case<span className="text-green-600">cobra</span>
                 </Link>
 
-                <div className="h-full flex items-center space-x-4"></div>
+                <div className="h-full flex items-center space-x-4">
+                    {user ? (<><Link href="/api/auth/logout" className={buttonVariants({
+                        size:"sm",
+                        variant:"ghost",
+                    })}></Link></>) :(<></>)}
+                </div>
             </div>
         </MaxWidthWrapper>
     </nav>
